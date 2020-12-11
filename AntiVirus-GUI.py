0011ff90b8f5753f0ec61c7d863443085aa12b36
Arnav-Hazra <54741339+Arnav-Hazra@users.noreply.github.com>
2020-12-11 19:08:55 +0530

    v1.02

--- AntiVirus-GUI.py
+++ AntiVirus-GUI.py
@@ -704,4 +704,5 @@ t_clock.start()
 time.sleep(5)
 
 # I commented this line because it starts the system scan by default whenever the antivirus is started, and it causes the system to lag
+# uncomment the following if want to system scan
 #t_files.start()
